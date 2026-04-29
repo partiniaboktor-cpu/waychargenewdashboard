80920fd41b2239d7a7e251b00d86c637574403f5
Pattou Emad <pattouemad@Partinias-MacBook.local>
2026-04-29 19:23:07 +0300

    charging stations page

--- src/RoutingApp.jsx
+++ src/RoutingApp.jsx
@@ -4,6 +4,7 @@ import Login from "./Pages/Login";
 import DashboardaHome from "./Pages/DashboardaHome";
 import Websitecontent from "./Pages/Websitecontent";
 import Appcontent from "./Pages/Appcontent";
+import Chargingstations from "./Pages/Chargingstations";
 
 const RoutingApp = () => {
   return (
@@ -13,6 +14,7 @@ const RoutingApp = () => {
         <Route path="/dashboard" element={<DashboardaHome />} />
         <Route path="/website-content" element={<Websitecontent />} />
         <Route path="/app-content" element={<Appcontent />} />
+        <Route path="/charging-stations" element={<Chargingstations />} />
       </Routes>
     </BrowserRouter>
   );
